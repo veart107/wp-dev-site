9667be79b0b6b643b4941bb0de8e321b92038426
veart107 <veart107@hsd.k12.or.us>
2016-04-20 12:46:03 -0700

    style.css,404.php,

---
+++ wp-content/themes/HTML5-Reset/404.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage HTML5-Reset-WordPress-Theme
+ * @since HTML5 Reset 2.0
+ */
+ get_header(); ?>
+
+	<h2><?php _e('Good going! You broke the Site, dunce!'); ?></h2>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
